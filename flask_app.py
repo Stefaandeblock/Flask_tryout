cf6cda5fb932e10379ac2b77ea44f8945acc9ab0
Stefaan De Block <stefaan.deblock.sd@gmail.com>
2024-10-02 20:43:31 +0200

    nieuwe tekst

--- flask_app.py
+++ flask_app.py
@@ -3,6 +3,6 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<p>Hello world!</p>"
+    return "<p>Flask code les 11!</p>"
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
